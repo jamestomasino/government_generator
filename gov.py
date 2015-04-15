a602ca000f22e0d3453e85db61ea89035098de44
James Tomasino <james@tomasino.org>
2015-04-15 11:41:56 -0400

    using cmd lib for python

--- gov.py
+++ gov.py
@@ -1,9 +1,69 @@
 #!/usr/local/bin/python
 
+import cmd
 import json
-json_data = open('gov.json')
-data = json.load(json_data)
 
-gov = data["government"];
-types = [ x["name"] for x in gov]
-print (types);
+class Government(cmd.Cmd):
+    """Simple government type generator."""
+
+    prompt = 'gov> '
+    intro = "Government Type Generator. Type 'help' for commands."
+
+    json_data = open('gov.json')
+    data = json.load(json_data)
+    gov = data["government"];
+    TYPES = [ x["name"] for x in gov]
+
+    def do_type(self, gov_type):
+        """type [gov_type]
+        Choose a type of government list to use."""
+        if gov_type:
+            print "gov_type chosen:", gov_type
+        else:
+            print 'no gov_type chosen'
+
+    def help_type(self):
+        print '\n'.join([ '',
+                          'type [gov_type]',
+                          '-- Choose a type of government list to use',
+                          ''
+                          ])
+
+    def do_rand(self, gov_type):
+        """rand [gov_type]
+        Generate a random government from the type chosen, or random if blank"""
+        if gov_type:
+            print "gov_type chosen:", gov_type
+        else:
+            print 'no gov_type chosen'
+
+    def help_rand(self):
+        print '\n'.join([ '',
+                          'rand [gov_type]',
+                          '-- Generate a random government from the type chosen, or random if blank',
+                          ''
+                          ])
+
+    def complete_rand(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.TYPES[:]
+        else:
+            completions = [ f
+                            for f in self.TYPES
+                            if f.startswith(text)
+                            ]
+        return completions
+
+    def do_exit(self, line):
+        "Exit"
+        return True
+
+    def do_EOF(self, line):
+        "Exit"
+        return True
+
+    def postloop(self):
+        print "\n"
+
+if __name__ == '__main__':
+    Government().cmdloop()
